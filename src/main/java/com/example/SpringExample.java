3fac2425dbd6a40f2aa19e48db4242af42ba7251
java <java@bigdata>
2017-09-04 18:15:45 +0900

    again and again

---
+++ src/main/java/com/example/SpringExample.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class SpringExample {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello Spring with git");
+		System.out.println("Hello Spring with git");
+	}
+
+}
